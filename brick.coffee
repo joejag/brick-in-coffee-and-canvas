b6a224a304a9b25841bd19aed699535d4fae6cf5
Joe Wright <joe@joejag.com>
2012-11-16 23:47:24 +0000

    Added bricks

--- brick.coffee
+++ brick.coffee
@@ -1,5 +1,32 @@
+#
+# Drawing
+#
+
 canvas = $('#brick')[0]
 context = canvas.getContext('2d')
+
+drawFilledRectangle = (d) ->
+    context.fillRect(d.cords.x, d.cords.y,
+                     d.dimensions.width, d.dimensions.height)
+
+drawStrokedRectangle = (d) ->
+    context.fillStyle = d.color
+    context.fillRect(d.cords.x, d.cords.y,
+                     d.dimensions.width, d.dimensions.height)
+    context.strokeRect(d.cords.x+1,d.cords.y+1,
+                       d.dimensions.width - 2, d.dimensions.height - 2)
+    
+
+drawArc = (drawable) ->
+    context.beginPath()
+    # Draw full circle, 0 to 2xPI
+    context.arc(drawable.cords.x, drawable.cords.y, drawable.radius,
+        0, Math.PI*2, true)
+    context.fill()
+
+#
+# Game world
+#
 
 class Cords
     constructor: (@x, @y) ->
@@ -16,19 +43,34 @@ class Ball
     cords: new Cords 300, 300
     radius: 10
 
-drawRectangle = (drawable) ->
-    context.fillRect(drawable.cords.x, drawable.cords.y,
-                     drawable.dimensions.width, drawable.dimensions.height)
+class Brick
+    constructor: (@base_cords, @width, @color) ->
+        @dimensions = new Dimensions(20, @width)
+        @cords = new Cords(@base_cords.x * @dimensions.width, @base_cords.y * @dimensions.height)
 
-drawArc = (drawable) ->
-    context.beginPath()
-    # Draw full circle, 0 to 2xPI
-    context.arc(drawable.cords.x, drawable.cords.y, drawable.radius,
-        0, Math.PI*2, true)
-    context.fill()
+#
+# Objects
+#
 
-p = new Paddle
-b = new Ball
+bricks_per_row = 8
+brick_width = canvas.width/bricks_per_row
 
-drawRectangle(p)
-drawArc(b)
+paddle = new Paddle
+ball = new Ball
+bricks = []
+for x in [0...bricks_per_row]
+  for y in [0...4]
+      bricks.push new Brick(new Cords(x,y), brick_width, _.shuffle(['orange', 'red','green'])[0])
+
+
+#
+# Draw world
+#
+
+drawFilledRectangle(paddle)
+drawArc(ball)
+drawStrokedRectangle(brick) for brick in bricks
+
+
+
+
--- brick.js
+++ brick.js
@@ -1,9 +1,25 @@
 (function() {
-  var Ball, Cords, Dimensions, Paddle, b, canvas, context, drawArc, drawRectangle, p;
+  var Ball, Brick, Cords, Dimensions, Paddle, ball, brick, brick_width, bricks, bricks_per_row, canvas, context, drawArc, drawFilledRectangle, drawStrokedRectangle, paddle, x, y, _i, _j, _k, _len;
 
   canvas = $('#brick')[0];
 
   context = canvas.getContext('2d');
+
+  drawFilledRectangle = function(d) {
+    return context.fillRect(d.cords.x, d.cords.y, d.dimensions.width, d.dimensions.height);
+  };
+
+  drawStrokedRectangle = function(d) {
+    context.fillStyle = d.color;
+    context.fillRect(d.cords.x, d.cords.y, d.dimensions.width, d.dimensions.height);
+    return context.strokeRect(d.cords.x + 1, d.cords.y + 1, d.dimensions.width - 2, d.dimensions.height - 2);
+  };
+
+  drawArc = function(drawable) {
+    context.beginPath();
+    context.arc(drawable.cords.x, drawable.cords.y, drawable.radius, 0, Math.PI * 2, true);
+    return context.fill();
+  };
 
   Cords = (function() {
 
@@ -53,22 +69,43 @@
 
   })();
 
-  drawRectangle = function(drawable) {
-    return context.fillRect(drawable.cords.x, drawable.cords.y, drawable.dimensions.width, drawable.dimensions.height);
-  };
+  Brick = (function() {
 
-  drawArc = function(drawable) {
-    context.beginPath();
-    context.arc(drawable.cords.x, drawable.cords.y, drawable.radius, 0, Math.PI * 2, true);
-    return context.fill();
-  };
+    function Brick(base_cords, width, color) {
+      this.base_cords = base_cords;
+      this.width = width;
+      this.color = color;
+      this.dimensions = new Dimensions(20, this.width);
+      this.cords = new Cords(this.base_cords.x * this.dimensions.width, this.base_cords.y * this.dimensions.height);
+    }
 
-  p = new Paddle;
+    return Brick;
 
-  b = new Ball;
+  })();
 
-  drawRectangle(p);
+  bricks_per_row = 8;
 
-  drawArc(b);
+  brick_width = canvas.width / bricks_per_row;
+
+  paddle = new Paddle;
+
+  ball = new Ball;
+
+  bricks = [];
+
+  for (x = _i = 0; 0 <= bricks_per_row ? _i < bricks_per_row : _i > bricks_per_row; x = 0 <= bricks_per_row ? ++_i : --_i) {
+    for (y = _j = 0; _j < 4; y = ++_j) {
+      bricks.push(new Brick(new Cords(x, y), brick_width, _.shuffle(['orange', 'red', 'green'])[0]));
+    }
+  }
+
+  drawFilledRectangle(paddle);
+
+  drawArc(ball);
+
+  for (_k = 0, _len = bricks.length; _k < _len; _k++) {
+    brick = bricks[_k];
+    drawStrokedRectangle(brick);
+  }
 
 }).call(this);
